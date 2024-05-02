d80fe769e92378ff0c5f3e1c43836a6e9ed5a9db
izzy <mista.rol@gmail.com>
2024-05-03 02:55:43 +0800

    sesuai mockup

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,106 @@
 import Image from "next/image";
+import Link from "next/link";
+
+// bg-[#ff451c]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="flex min-h-screen flex-col items-center justify-between p-10 bg-[#ff451c]">
+      <div className="text-center items-center flex flex-col gap-8">
+        <div
+          id="heading"
+          className="flex flex-col justify-center items-center pt-4 gap-8 md:gap-16"
+        >
+          <div className="max-w-[50vw] md:max-w-[33vw]">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="LOGO_MAC_AMPM.svg"
+              width={1024}
+              height={1024}
+              alt="Monday Art Club & AMPM logo"
             />
-          </a>
+          </div>
+          <div>
+            <Image
+              src="GRAPHIC_COTF.svg"
+              width={1024}
+              height={1024}
+              alt="Classics of The Future"
+            />
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+        <div className="text-xl">
+          AT KLIF
+          <br />
+          3–4 MAY 2024
+          <br />
+          GMBB
+        </div>
+        <div id="paragraph" className="flex flex-col gap-4 text-xs max-w-md">
+          <p>
+            &quot;Classics of the Future&quot; is the conceptual theme we intend
+            to bring to KLIF. In this upcoming exhibition, artists will be
+            challenged to produce an artifact inspired by their work found 1000
+            years in the future.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p>
+            We will also feature a small booth showcasing the works of every
+            artist exhibiting in our venue, including posters, sticker packs,
+            and more!
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div id="sponsors" className="flex flex-col items-center mt-4 gap-4">
+          <span>SPONSORS</span>
+          <Image
+            src="LOGO_ZIPPO_EXCHANGEART.svg"
+            width={1024}
+            height={1024}
+            alt="Zippo & Art Exchange logo"
+            className="max-w-64 sm:max-w-72"
+          />
+        </div>
+        <div id="printing" className="flex flex-col items-center mt-8 gap-4">
+          <span>PRINTING PARTNER</span>
+          <Image
+            src="LOGO_FENESTRA.svg"
+            width={1024}
+            height={1024}
+            alt="Fenestra Imaging logo"
+            className="max-w-28 sm:max-w-36 mt-2"
+          />
+        </div>
+        <div id="footer" className="w-full mt-16 p-10 flex gap-4 items-center">
+          <div className="w-4/5 text-left text-xs md:text-sm">
+            Monday Art Club is an artist collective based in Indonesia
+            <br />
+            Web3, Physical, and Beyond
+          </div>
+          <div
+            id="socials"
+            className="md:flex-row w-1/5 flex flex-col items-end justify-end gap-3"
+          >
+            <Link href="https://twitter.com/mondayartclub/" target="_blank">
+              <Image
+                src="/x.svg"
+                width={256}
+                height={256}
+                alt="X"
+                className="w-6"
+              />
+            </Link>{" "}
+            <Link
+              href="https://www.instagram.com/themondayartclub/"
+              target="_blank"
+            >
+              <Image
+                src="/ig.svg"
+                width={256}
+                height={256}
+                alt="Instagram"
+                className="w-6"
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   );
